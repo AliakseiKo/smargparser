9e05ecfeb10e2791853801b55bb23f3e17cd253b
Aliaksei <AliakseiKo@gmail.com>
2020-09-21 21:14:07 +0300

    add base functionality

--- index.js
+++ index.js
@@ -1,15 +1,96 @@
+/**
+ * Creates an object from the results of the callback call. Works like Array.prototype.map.
+ * @param {Array.<string>} [args=process.argv] - string array of arguments.
+ * @param {boolean} [negativeNumbers=true] - the flag specifies whether to parse a number with
+ * prefix as a number or argument (example -10, minus treated as a prefix).
+ * Following arguments '-a -10' will be parsed as
+ * if true: { a: -10 };
+ * if false: { a: true, 1: true, 0: true };
+ * @returns {Object.<string, *>} - an argument object argv with the following
+ * structure: { <key>: <value> } filled with the array arguments from args.
+ * argv._ - contains all arguments that are not related to others.
+ * argv['--'] - contains all arguments after --.
+ * @example
+ * parseArgs(['--foo=25', '-abc', '-10']);
+ *
+ * returns { foo: '25', a: true, b: true, c: -10 };
+ */
+function parseArgs(argv = process.argv.slice(2), negativeNumbers = true) {
+  const resultDict = {};
+  const _ = [];
+  let __ = [];
+
+  for (let i = 0; i < argv.length;) {
+    if (argv[i] === '--') {
+      __ = argv.slice(i + 1);
+      break;
+    } else if (argv[i].slice(0, 2) === '--') {
+      let { key, value } = parseKeyValue(argv[i++].slice(2));
+
+      if (value === null) {
+        ({ i, value } = readValue(i, argv, negativeNumbers));
+      }
+
+      resultDict[key] = parseValue(value);
+    } else if (argv[i].charAt(0) === '-' && !(negativeNumbers && /^[+-]?\d+(?:\.\d+|e[+-]\d+)?/.test(argv[i]))) {
+
+      const keys = argv[i].slice(1).split('');
+      let key, value;
+
+      for (let j = 0; j < keys.length; ++j) {
+        key = keys[j];
+        resultDict[key] = true;
+      }
+
+      ({ i, value } = readValue(++i, argv, negativeNumbers));
+
+      resultDict[key] = value;
+    } else {
+      _.push(parseValue(argv[i++]));
+    }
+  }
+
+  resultDict._ = _;
+  resultDict['--'] = __;
+
+  return resultDict;
+}
+
+function parseValue(value) {
+  try {
+    return JSON.parse(value);
+  } catch (error) {
+    return value;
+  }
+}
+
 function parseKeyValue(arg) {
   const sign = arg.lastIndexOf('=');
   const isSign = Boolean(~sign);
-  let key = (isSign) ? arg.slice(2, sign) : arg.slice(2);
-  let value = (isSign) ? arg.slice(sign + 1) : true;
+  let key = (isSign) ? arg.slice(0, sign) : arg;
+  let value = (isSign) ? arg.slice(sign + 1) : null;
 
   if (key === '') {
     if (isSign) key = '=' + value;
-    value = true;
+    value = null;
   }
 
   return { key, value };
 }
 
-module.exports = { parseKeyValue };
+function readValue(i, argv, negativeNumbers) {
+  const buffer = [];
+  for (; argv[i] && (negativeNumbers && /^[+-]?\d+(?:\.\d+|e[+-]\d+)?/.test(argv[i]) || argv[i].charAt(0)) !== '-' ; ++i) {
+    buffer.push(parseValue(argv[i]));
+  }
+
+  const value = (buffer.length === 0)
+    ? true
+    : (buffer.length === 1)
+      ? buffer[0]
+      : buffer;
+
+  return { i, value };
+}
+
+module.exports = parseArgs;
--- index.test.js
+++ index.test.js
@@ -1,12 +1,53 @@
 /* eslint-env jest */
 
-const { parseKeyValue } = require('./index');
+const parse = require('./index');
 
-test('must parse', () => {
-  expect(parseKeyValue('--key=value')).toEqual({ key: 'key', value: 'value' });
-  expect(parseKeyValue('--key=')).toEqual({ key: 'key', value: '' });
-  expect(parseKeyValue('--key')).toEqual({ key: 'key', value: true });
-  expect(parseKeyValue('--=value')).toEqual({ key: '=value', value: true });
-  expect(parseKeyValue('--=')).toEqual({ key: '=', value: true });
-  expect(parseKeyValue('--')).toEqual({ key: '', value: true });
+describe(parse, () => {
+  test.each([
+    ['-a'.split(' '),             { _: [], '--': [], a: true }],
+    ['-a -b -c'.split(' '), { _: [], '--': [], a: true, b: true, c: true }],
+    ['-abc'.split(' '),           { _: [], '--': [], a: true, b: true, c: true }],
+  ])('must parse %s correctly', (arg, expected) => {
+    expect(parse(arg)).toEqual(expected);
+  });
+
+  test.each([
+    ['-a 5000'.split(' '),         { _: [], '--': [], a: 5000 }],
+    ['-a 1 -b 2 -c 3'.split(' '),  { _: [], '--': [], a: 1, b: 2, c: 3 }],
+    ['-a 1 true null'.split(' '),  { _: [], '--': [], a: [1, true, null] }],
+    ['-a -b -c hello!'.split(' '), { _: [], '--': [], a: true, b: true, c: 'hello!' }],
+    ['-abc hello!'.split(' '),     { _: [], '--': [], a: true, b: true, c: 'hello!' }],
+  ])('must parse %s correctly', (arg, expected) => {
+    expect(parse(arg)).toEqual(expected);
+  });
+
+  test.each([
+    ['-a -10'.split(' '),                   { _: [], '--': [], a: -10 }],
+    ['-a -10 4e-2 -0.25'.split(' '),          { _: [], '--': [], a: [-10, 4e-2, -0.25] }],
+    ['--foo= null'.split(' '),              { _: [null], '--': [], foo: '' }],
+    ['--foo null'.split(' '),               { _: [], '--': [], foo: null }],
+    ['--foo null 10'.split(' '),            { _: [], '--': [], foo: [null, 10] }],
+    ['--foo -ab 5 --bar 5 6 --baz -- --qux 25 -ab'.split(' '),
+    { _: [], '--': ['--qux', '25', '-ab'], foo: true, a: true, b: 5, bar: [5, 6], baz: true }],
+  ])('must parse %s correctly', (arg, expected) => {
+    expect(parse(arg)).toEqual(expected);
+  });
+
+  test.each([
+    [['--foo=[null, true, false, -5, 0, 2.25, "hello", [1,2,3], { "name": "vasiliy", "age": 25 }]'],
+    { _: [], '--': [], foo: [null, true, false, -5, 0, 2.25, "hello", [1, 2, 3], { name: 'vasiliy', age: 25 }] }],
+
+    ['-a 5.5 -bcd false true --foo=null 10 3000 -p false'.split(' '),
+    { _: [10, 3000], '--': [], a: 5.5, b: true, c: true, d: [false, true], foo: null, p: false }],
+  ])('must parse %s correctly', (arg, expected) => {
+    expect(parse(arg)).toEqual(expected);
+  });
+
+  test.each([
+    ['-a -10'.split(' '), false, { _: [], '--': [], a: true, '1': true, '0': true }],
+    ['-a -10 -20 -0.25'.split(' '), false,
+    { _: [], '--': [], a: true, '1': true, '0': true, '2': true, '.': true, '5': true }],
+  ])('must parse %s with negativeNumber is true correctly', (arg, negativeNumbers, expected) => {
+    expect(parse(arg, negativeNumbers)).toEqual(expected);
+  });
 });
